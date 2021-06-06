8d2c9dd23f02c3f4dabc615c59e00fd9087a0d2f
takashi.aihara <takashi.aihara@monocrea.co.jp>
2021-06-06 16:37:18 +0900

    first commit

---
+++ src/test/java/com/code/innovate/rssreader/RssreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.innovate.rssreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
